81b615f4db0fd2c99d046320d2380b84209e6713
Ziad Ahmed <102294610+z1adAhmed@users.noreply.github.com>
2023-09-16 14:44:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include"BankAccount.h"
+#include"SavingsBankAccount.h"
+#include"Client.h"
+#include"BankingApplication.h"
+using namespace std;
+
+
+
+
+int main()
+{
+    BankingApplication b1;
+    b1.run();
+//    BankAccount b1(2000);
+//    BankAccount * b1_ptr = &b1;
+    //Client c1;
+//    SavingsBankAccount s1(500,120);
+//    cout<<s1.get_account_id()<<endl<<s1.get_balance()<<endl<<s1.get_minimumBalance()<<endl;
+//    c1.set_name("hazem");
+//    c1.set_adress("6 october");
+//    c1.set_phoneNumber("01007831795");
+//    c1.set_bankAcc(b1_ptr);
+//    cout<<c1.get_name()<<endl<<c1.get_adress()<<endl<<c1.get_phoneNumber()<<endl<<c1.get_bankAcc()->get_balance();
+
+    return 0;
+}
